7f557d2e099df3bfc3dc79943f3214adad36b73d
THEMPGUYActions <biglolxdbruh@gmail.com>
2026-09-16 08:29:41 -0400

    Fix honeypot compatibility to use security configuration

--- modules/honeypot/index.js
+++ modules/honeypot/index.js
@@ -1,1 +1,12 @@
-module.exports = require('../security');
+const security = require('../security');
+
+async function initialize(client, config, moduleConfig, advanced) {
+  return security.initialize(client, config, config.security || moduleConfig || {}, advanced);
+}
+
+module.exports = {
+  initialize,
+  commands: security.commands,
+  contextMenus: security.contextMenus,
+  listeners: security.listeners
+};
